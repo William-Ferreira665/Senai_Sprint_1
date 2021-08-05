54bd07c3160d482db5b1a64070b4459ab618d7f0
William-Ferreira665 <82423153+William-Ferreira665@users.noreply.github.com>
2021-08-04 21:22:08 -0300

    Add files via upload

---
+++ 2021-2S-2-DM/Sprint 1/Exercícios/1.0-exercicio-filmes/1.0-exercicio-filmes.sql
@@ -0,0 +1,33 @@
+--Este é um comentário
+/*
+Comentário em várias linhas mó paz
+Vida boa sou programador
+*/
+
+--atalho
+--escrever ctrl + k depois ctrl +  c
+
+--COMANDOS DDL
+
+--CREATE
+
+CREATE DATABASE CATALOGO;
+GO
+
+USE CATALOGO;
+GO
+
+CREATE TABLE GENERO(
+	idGenero TINYINT PRIMARY KEY IDENTITY(1,1),
+	nomeGenero VARCHAR(30)
+);
+GO
+
+CREATE TABLE FILME(
+	idFilme SMALLINT PRIMARY KEY IDENTITY(1,1),
+	idGenero TINYINT FOREIGN KEY REFERENCES GENERO(idGenero),
+	tituloFilme VARCHAR(70)
+);
+GO
+
+--ALT + F1 ANALISA TABELA
